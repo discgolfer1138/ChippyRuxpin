1559382bf73f6f11c79037fdaacd8161f65e3f15
Chad Francis <chad.francis@mapquest.com>
2016-12-07 11:45:22 -0700

    trying to fix gpio issue

--- chippyRuxpin_gpio.py
+++ chippyRuxpin_gpio.py
@@ -16,7 +16,7 @@ class GPIO:
     def setup(self,pin):
         cmd = "echo " + str(pin) + " > /sys/class/gpio/export"
         subprocess.call(cmd,shell=True, stdout=subprocess.PIPE)
-        cmd = "echo \"out\" > /sys/class/gpio/gpio" + str(pin) + "/direction" 
+        cmd = "echo \"out\" > /sys/class/gpio/gpiochip" + str(pin) + "/direction" 
         subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE)
         self.pins.append([pin,0,0])
         self.set( pin, 0 )
@@ -26,12 +26,12 @@ class GPIO:
             for pinObject in self.pins:
                 if(pinObject[0]==pin and pinObject[1]!=val):
                     pinObject[1]=val
-                    cmd = "echo " + str(val) + " > /sys/class/gpio/gpio" + str(pinObject[0]) + "/value"
+                    cmd = "echo " + str(val) + " > /sys/class/gpio/gpiochip" + str(pinObject[0]) + "/value"
                     subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE)
         
     def cleanup(self):
         for pinObject in self.pins:
-            cmd = "echo 0 > /sys/class/gpio/gpio" + str(pinObject[0]) + "/value"
+            cmd = "echo 0 > /sys/class/gpio/gpiochip" + str(pinObject[0]) + "/value"
             subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE)
             cmd = "echo " + str(pinObject[0]) + " > /sys/class/gpio/unexport"
             subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE)

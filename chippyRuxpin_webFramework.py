6f5465ba7492b68401a8e888f0a87026e1063e8c
Chad Francis <chad.francis@mapquest.com>
2016-12-14 16:03:28 -0700

    moving html to template file

--- chippyRuxpin_webFramework.py
+++ chippyRuxpin_webFramework.py
@@ -3,7 +3,7 @@
 # Chippy Ruxpin by Next Thing Co 2015
 # Powered by C.H.I.P., the world's first $9 computer!
 
-from bottle import run, get, post, request, route, redirect
+from bottle import run, get, post, request, route, redirect, template
 import socket
 
 class WebFramework:
@@ -19,50 +19,8 @@ class WebFramework:
         
         @route('/')
         def index():
-            return '''
-                <!doctype html>
-                <html lang="en">
-                    <head>
-                        <meta charset="utf-8">
-                        <title>Chippy Ruxpin</title>
-                        <meta name="description" content="Make the bear talk">
-                        <meta name="author" content="Chad Francis">
-                        <!-- Latest compiled and minified CSS -->
-                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-                        <!-- Optional theme -->
-                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-                    </head>
-                    <body>
-                        <form action="/" method="post">
-                            <small>What do you want Chippy Ruxpin to say? (Or type \"twitter\" followed by some search terms)<small>
-                            <div class="form-group">
-                                <label for="speech">Text to say</label>
-                                <input type="text" name="speech" class="form-control" id="speech" placeholder="I have a pony, he takes big shits.">
-                            </div>
-                            <div class="form-group">
-                                <label for="phrase">Canned Phrase</label>
-                                <select name="phrase" class="form-control" id="phrase">
-                                    <option value="">none</option>
-                                    <option value="believeitornot">doggo: believe it or not...</option>
-                                    <option value="bitch">say what again...</option>
-                                    <option value="breath">vader: breathing</option>
-                                    <option value="dogofwisdom">doggo: ba daba da ba</option>
-                                    <option value="failed">vader: you have failed me for the last time</option>
-                                    <option value="faith">vader: I find your lack of faith disturbing</option>
-                                    <option value="father">vader: No, I am your father</option>
-                                    <option value="honored">vader: We'd be honored...</option>
-                                    <option value="merryxmas">clark: boss rant</option>
-                                    <option value="nerfherder">leia: why you stuck up...</option>
-                                    <option value="proud">vader: don't be too proud...</option>
-                                    <option value="surprised">clark: if I woke up tomorrow...</option>
-                                </select>
-                            </div>
-                            <button type="submit" class="btn btn-default">Go!</button>
-                            <button type="submit" name="demo" value="1" class="btn btn-default">Directions Demo</button>
-                        </form>
-                    </body>
-                </html>
-            '''
+            return template('index')
+
         @post('/')
         def speak():
             phrase = request.forms.get('phrase')

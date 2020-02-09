592ed4bc68c1d9933a49fcefb763ee7a8254ece0
Chad Francis <yamanote1138@gmail.com>
2020-02-08 23:51:37 -0700

    fixing indenting

--- main.py
+++ main.py
@@ -46,38 +46,38 @@ audio = None
 isRunning = True
 
 def updateMouth():
-	lastMouthEvent = 0
-	lastMouthEventTime = 0
+  lastMouthEvent = 0
+  lastMouthEventTime = 0
 
-	while( audio == None ):
-		time.sleep( 0.1 )
-		
-	while isRunning:
-		if( audio.mouthValue != lastMouthEvent ):
-			lastMouthEvent = audio.mouthValue
-			lastMouthEventTime = time.time()
+  while( audio == None ):
+    time.sleep( 0.1 )
+    
+  while isRunning:
+    if( audio.mouthValue != lastMouthEvent ):
+      lastMouthEvent = audio.mouthValue
+      lastMouthEventTime = time.time()
 
-			if( audio.mouthValue == 1 ):
-				mouth.open()
-			else:
+      if( audio.mouthValue == 1 ):
+        mouth.open()
+      else:
         mouth.close()
-		else:
-			if( time.time() - lastMouthEventTime > 0.4 ):
-				mouth.stop()
+    else:
+      if( time.time() - lastMouthEventTime > 0.4 ):
+        mouth.stop()
 
 # A routine for blinking the eyes in a semi-random fashion.
 def updateEyes():
-	while isRunning:
-		eyes.close()
+  while isRunning:
+    eyes.close()
     eyes.open()
-		time.sleep( randint( 0,7) )
+    time.sleep( randint( 0,7) )
    
 def talk(myText):
-	os.system( "espeak \",...\" 2>/dev/null" ) # Sometimes the beginning of audio can get cut off. Insert silence.
-	time.sleep( 0.5 )
-	subprocess.call(["espeak", "-w", "speech.wav", myText, "-s", "130"])
-	audio.play("speech.wav")
-	return myText
+  os.system( "espeak \",...\" 2>/dev/null" ) # Sometimes the beginning of audio can get cut off. Insert silence.
+  time.sleep( 0.5 )
+  subprocess.call(["espeak", "-w", "speech.wav", myText, "-s", "130"])
+  audio.play("speech.wav")
+  return myText
 
 mouthThread = Thread(target=updateMouth)
 mouthThread.start()
